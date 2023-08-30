ee2777baf7d1d1baec9e86514549633d51ddb42f
joshuaPhong <110524932+joshuaPhong@users.noreply.github.com>
2023-08-30 20:03:20 +1200

    made a mess

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
 
     path('', views.all_blogs, name='all_blogs'),
+    # looks for an int after blog. call it blog_id
+    path('<int:blog_id>', views.detail, name='detail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogModel
 
 
 # Create your views here.
 
 def all_blogs(request):
-    blog_object = BlogModel.objects.all()
+    # we can set limits slice the blog object
+    # and order by reverse date (newest first).
+    blog_object = BlogModel.objects.order_by('-date')[0:5]
+    # blog_object = BlogModel.objects.all()
     return render(request, 'blog/all_blogs.html', {'blogObject': blog_object})
+
+
+def detail(request, blog_id):
+    # grabs the blogid from the blog model or returns a 404
+    blog = get_object_or_404(BlogModel, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
